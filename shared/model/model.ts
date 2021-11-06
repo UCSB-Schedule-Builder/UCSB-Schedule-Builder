a4cc06e629013e799fc7afc8451217c031efd106
jacksonjude <jacksonjude@mac.com>
2021-11-06 14:47:01 -0700

    Added source comment for model.ts idea

--- shared/model/model.ts
+++ shared/model/model.ts
@@ -1,4 +1,5 @@
 // Always import model classes from here to avoid circular import issues
+// Inspired by https://medium.com/visual-development/how-to-fix-nasty-circular-dependency-issues-once-and-for-all-in-javascript-typescript-a04c987cf0de
 
 export * from "./Subject"
 export * from "./Course"
