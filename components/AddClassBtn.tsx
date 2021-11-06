388e34f6ff3b2bc342b984e56f0fbc468081c75b
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2021-11-06 05:19:17 -0700

    Polish classes list UI a bit

--- components/AddClassBtn.tsx
+++ components/AddClassBtn.tsx
@@ -1,37 +1,40 @@
-import grid from '../constants/classList';
-import { ClassListType } from '../stores/classLists';
-import useSearch from '../stores/classSearch';
+import grid from "../constants/classList";
+import { ClassListType } from "../stores/classLists";
+import useSearch from "../stores/classSearch";
 
 function AddClassBtn({ classType }: { classType: ClassListType }) {
-  const search = useSearch(state => state.search);
+  const search = useSearch((state) => state.search);
 
   function handleAddClass() {
     search(classType);
   }
 
-  return <>
-    <button onClick={handleAddClass}>+ Add Class</button>
-    <style jsx>{`
-      button {
-        border-radius: 10px;
-        background-color: #f9f9f9;
-        color: #5e6c84;
-        border: none;
-        width: 100%;
-        padding: 0.4rem;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: -${grid * .25}px 0 ${grid * .75}px;
-      }
+  return (
+    <>
+      <button onClick={handleAddClass}>+ Add Class</button>
+      <style jsx>{`
+        button {
+          border-radius: 10px;
+          background-color: #f9f9f9;
+          color: #5e6c84;
+          border: none;
+          width: 100%;
+          padding: 0.4rem;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 16px;
+          margin: -${grid * 0.25}px 0 ${grid * 0.75}px;
+        }
 
-      button:hover {
-        background-color: #091e4214;
-        color: #172b4d;
-      }
-    `}</style>
-  </>
+        button:hover {
+          background-color: #091e4214;
+          color: #172b4d;
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default AddClassBtn;
--- components/ClassCard.tsx
+++ components/ClassCard.tsx
@@ -1,75 +1,118 @@
-import { Draggable, DraggingStyle, NotDraggingStyle } from 'react-beautiful-dnd';
-import { CSSProperties } from 'react';
+import {
+  Draggable,
+  DraggingStyle,
+  NotDraggingStyle,
+} from "react-beautiful-dnd";
+import { CSSProperties } from "react";
 
-import { Class } from '../stores/classLists';
-import grid from '../constants/classList';
+import { Class } from "../stores/classLists";
+import grid from "../constants/classList";
 
-const getClassStyle =
-  (isDragging: boolean, draggableStyle?: DraggingStyle | NotDraggingStyle): CSSProperties => ({
-    // some basic styles to make the class items look a bit nicer
-    userSelect: "none",
-    padding: grid * 2,
-    margin: `0 0 ${grid * 1.5}px 0`,
+const getClassStyle = (
+  isDragging: boolean,
+  draggableStyle?: DraggingStyle | NotDraggingStyle
+): CSSProperties => ({
+  // some basic styles to make the class items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid * 1.5}px 0`,
 
-    // change background colour if dragging
-    background: isDragging ? "lightgreen" : "white",
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "white",
 
-    boxShadow: "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.20)",
+  boxShadow:
+    "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.20)",
 
-    borderRadius: 10,
+  borderRadius: 10,
 
-    // styles we need to apply on draggables
-    ...draggableStyle
-  });
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
 
 function ClassCard({
-                     classData,
-                     index,
-                     removeAt
-                   }: { classData: Class, index: number, removeAt: (index: number) => void }) {
+  classData,
+  index,
+  removeAt,
+}: {
+  classData: Class;
+  index: number;
+  removeAt: (index: number) => void;
+}) {
   const { id, content } = classData;
-  return <>
-    <Draggable
-      draggableId={id}
-      index={index}
-    >
-      {({ dragHandleProps, draggableProps, innerRef }, snapshot) => (
-        <div
-          ref={innerRef}
-          {...draggableProps}
-          {...dragHandleProps}
-          style={getClassStyle(
-            snapshot.isDragging,
-            draggableProps.style
-          )}
-        >
-          <div className="card"
+  return (
+    <>
+      <Draggable draggableId={id} index={index}>
+        {({ dragHandleProps, draggableProps, innerRef }, snapshot) => (
+          <div
+            ref={innerRef}
+            {...draggableProps}
+            {...dragHandleProps}
+            style={getClassStyle(snapshot.isDragging, draggableProps.style)}
           >
-            {content}
-            <button
-              type="button"
-              onClick={() => {
-                removeAt(index);
-              }}
-            >
-              delete
-            </button>
+            <div className="card">
+              <a
+                href="#"
+                className="close"
+                onClick={() => {
+                  removeAt(index);
+                }}
+              >
+                <p className="close-inner">&times;</p>
+              </a>
+              {content}
+            </div>
           </div>
-        </div>
-      )}
-    </Draggable>
-    <style jsx>{`
-      .card {
-        display: flex;
-        justify-content: space-around;
-        white-space: pre-wrap;
-      }
+        )}
+      </Draggable>
+      <style jsx>{`
+        div {
+          cursor: move;
+        }
 
-      * {
-        color: #172b4d;
-      }
-    `}</style>
-  </>
+        .card {
+          position: relative;
+          display: inline-block;
+          //display: flex;
+          justify-content: space-around;
+          white-space: pre-wrap;
+        }
+
+        * {
+          color: #172b4d;
+        }
+
+        .close {
+          position: absolute;
+          top: -25px;
+          left: -25px;
+          z-index: 100;
+          background-color: #dc143c;
+          padding: 8px 5px 5px;
+          cursor: pointer;
+          opacity: 0;
+          border-radius: 50%;
+        }
+
+        .close-inner {
+          position: relative;
+          top: -3px;
+          margin: 0;
+          color: #fff;
+          text-align: center;
+          font-size: 20px;
+          line-height: 10px;
+        }
+
+        div:hover .close {
+          opacity: 0.2;
+        }
+
+        .card .close:hover {
+          opacity: 1;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default ClassCard;
