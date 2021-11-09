354b110a3a04e075c1c8ae4cd0e2fd0c045aca02
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2021-11-09 00:18:56 -0800

    Format and clean up api-manager and api-tests

--- api/api-manager.ts
+++ api/api-manager.ts
@@ -5,122 +5,199 @@
 // 4. Create Course classes (+ UnitsRange objects)
 // 5. Pass Section classes into Course classes via createLectures
 
-import { Subject, Course, YearQuarter, CourseID, Section } from "../shared/model/model"
-import ky from "ky-universal"
-import { UCSBAPIKey, UCSBRegistrarAPIVersion, UCSBAPIPaths } from "../constants/apiConsts"
+import {
+  Course,
+  CourseID,
+  Section,
+  Subject,
+  YearQuarter,
+} from "../shared/model/model";
+import ky from "ky-universal";
+import {
+  UCSBAPIKey,
+  UCSBAPIPaths,
+  UCSBRegistrarAPIVersion,
+} from "../constants/apiConsts";
 
-export class APIManager
-{
-  static async fetchSubjects(): Promise<Subject[]>
-  {
-    var subjectsJSON: any = await ky.get(UCSBAPIPaths.subjects, {headers: {
-      "ucsb-api-version": UCSBRegistrarAPIVersion
-    }}).json()
+export class APIManager {
+  static async fetchSubjects(): Promise<Subject[]> {
+    const subjectsJSON: any = await ky
+      .get(UCSBAPIPaths.subjects, {
+        headers: {
+          "ucsb-api-version": UCSBRegistrarAPIVersion,
+        },
+      })
+      .json();
 
     return subjectsJSON.map((subjectJSON: any) => {
       return new Subject(
         subjectJSON.subjectCode,
         subjectJSON.subjectTranslation
-      )
-    })
+      );
+    });
   }
 
-  static async fetchCourses(quarter: YearQuarter, subject: Subject, shouldFetchSectionData: boolean = false): Promise<Course[]>
-  {
-    var coursesSearchJSON = await APIManager.fetchFromUCSBAPI(UCSBAPIPaths.classSearch, {searchParams: {
-      quarter: quarter.toString(),
-      subjectCode: subject.code,
-      includeClassSections: shouldFetchSectionData,
-      pageSize: 500
-    }, headers: {
-      "ucsb-api-key": UCSBAPIKey
-    }}, "classes")
-    if (!coursesSearchJSON || !coursesSearchJSON.classes) { return [] }
-    var coursesJSON = coursesSearchJSON.classes
-
-    var courses = coursesJSON.map((courseJSON: any) => {
-      return APIManager.handleCourseJSON(courseJSON, subject, shouldFetchSectionData)
-    })
-
-    return courses
-  }
-
-  static async fetchCourse(quarter: YearQuarter, id: CourseID, subject: Subject, shouldFetchSectionData: boolean = true): Promise<Course | null>
-  {
-    var coursesJSON = await APIManager.fetchFromUCSBAPI(UCSBAPIPaths.classSearch, {searchParams: {
-      quarter: quarter.toString(),
-      courseId: id.toString(),
-      includeClassSections: shouldFetchSectionData,
-      pageSize: 500
-    }, headers: {
-      "ucsb-api-key": UCSBAPIKey
-    }}, "classes")
-    if (!coursesJSON || !coursesJSON.classes || coursesJSON.classes.length == 0) { return null }
-    var courseJSON = coursesJSON.classes[0]
-
-    return APIManager.handleCourseJSON(courseJSON, subject, shouldFetchSectionData)
-  }
-
-  static async fetchCourseFromObject(course: Course, shouldFetchSectionData: boolean = true): Promise<Course | null>
-  {
-    return await APIManager.fetchCourse(course.quarter, course.id, course.subject, shouldFetchSectionData)
-  }
-
-  static async fetchCourseSections(course: Course): Promise<Section[]>
-  {
-    var coursesJSON = await APIManager.fetchFromUCSBAPI(UCSBAPIPaths.classSearch, {searchParams: {
-      quarter: course.quarter.toString(),
-      courseId: course.id.toString(),
-      includeClassSections: true,
-      pageSize: 500
-    }, headers: {
-      "ucsb-api-key": UCSBAPIKey
-    }}, "classes")
-    if (!coursesJSON || !coursesJSON.classes || coursesJSON.classes.length == 0) { return [] }
-    var courseJSON = coursesJSON.classes[0]
-
-    return APIManager.handleCourseSectionsJSON(courseJSON.classSections)
-  }
-
-  static async fetchFromUCSBAPI(url: string, options: any, fieldToAccumulate: string): Promise<any>
-  {
-    var fullJSON: any = null
-
-    while (fullJSON == null || fullJSON.pageNumber*fullJSON.pageSize < fullJSON.total)
-    {
-      options.searchParams.pageNumber = (options.searchParams.pageNumber || 0) + 1
-      var currentJSON: any = await ky.get(url, options).json()
-
-      if (fullJSON == null)
+  static async fetchCourses(
+    quarter: YearQuarter,
+    subject: Subject,
+    shouldFetchSectionData: boolean = false
+  ): Promise<Course[]> {
+    const coursesSearchJSON = await APIManager.fetchFromUCSBAPI(
+      UCSBAPIPaths.classSearch,
       {
-        fullJSON = currentJSON
-        continue
+        searchParams: {
+          quarter: quarter.toString(),
+          subjectCode: subject.code,
+          includeClassSections: shouldFetchSectionData,
+          pageSize: 500,
+        },
+        headers: {
+          "ucsb-api-key": UCSBAPIKey,
+        },
+      },
+      "classes"
+    );
+    if (!coursesSearchJSON || !coursesSearchJSON.classes) {
+      return [];
+    }
+    const coursesJSON = coursesSearchJSON.classes;
+
+    return coursesJSON.map((courseJSON: any) => {
+      return APIManager.handleCourseJSON(
+        courseJSON,
+        subject,
+        shouldFetchSectionData
+      );
+    });
+  }
+
+  static async fetchCourse(
+    quarter: YearQuarter,
+    id: CourseID,
+    subject: Subject,
+    shouldFetchSectionData: boolean = true
+  ): Promise<Course | null> {
+    const coursesJSON = await APIManager.fetchFromUCSBAPI(
+      UCSBAPIPaths.classSearch,
+      {
+        searchParams: {
+          quarter: quarter.toString(),
+          courseId: id.toString(),
+          includeClassSections: shouldFetchSectionData,
+          pageSize: 500,
+        },
+        headers: {
+          "ucsb-api-key": UCSBAPIKey,
+        },
+      },
+      "classes"
+    );
+    if (
+      !coursesJSON ||
+      !coursesJSON.classes ||
+      coursesJSON.classes.length === 0
+    ) {
+      return null;
+    }
+    const courseJSON = coursesJSON.classes[0];
+
+    return APIManager.handleCourseJSON(
+      courseJSON,
+      subject,
+      shouldFetchSectionData
+    );
+  }
+
+  static async fetchCourseFromObject(
+    course: Course,
+    shouldFetchSectionData: boolean = true
+  ): Promise<Course | null> {
+    return await APIManager.fetchCourse(
+      course.quarter,
+      course.id,
+      course.subject,
+      shouldFetchSectionData
+    );
+  }
+
+  static async fetchCourseSections(course: Course): Promise<Section[]> {
+    const coursesJSON = await APIManager.fetchFromUCSBAPI(
+      UCSBAPIPaths.classSearch,
+      {
+        searchParams: {
+          quarter: course.quarter.toString(),
+          courseId: course.id.toString(),
+          includeClassSections: true,
+          pageSize: 500,
+        },
+        headers: {
+          "ucsb-api-key": UCSBAPIKey,
+        },
+      },
+      "classes"
+    );
+    if (
+      !coursesJSON ||
+      !coursesJSON.classes ||
+      coursesJSON.classes.length === 0
+    ) {
+      return [];
+    }
+    const courseJSON = coursesJSON.classes[0];
+
+    return APIManager.handleCourseSectionsJSON(courseJSON.classSections);
+  }
+
+  static async fetchFromUCSBAPI(
+    url: string,
+    options: any,
+    fieldToAccumulate: string
+  ): Promise<any> {
+    let fullJSON: any = null;
+
+    while (
+      fullJSON == null ||
+      fullJSON.pageNumber * fullJSON.pageSize < fullJSON.total
+    ) {
+      options.searchParams.pageNumber =
+        (options.searchParams.pageNumber || 0) + 1;
+      const currentJSON: any = await ky.get(url, options).json();
+
+      if (fullJSON == null) {
+        fullJSON = currentJSON;
+        continue;
       }
 
-      fullJSON[fieldToAccumulate].push.apply(fullJSON[fieldToAccumulate], currentJSON[fieldToAccumulate])
-      fullJSON.pageNumber = currentJSON.pageNumber
+      fullJSON[fieldToAccumulate].push.apply(
+        fullJSON[fieldToAccumulate],
+        currentJSON[fieldToAccumulate]
+      );
+      fullJSON.pageNumber = currentJSON.pageNumber;
     }
 
-    return fullJSON
+    return fullJSON;
   }
 
-  static handleCourseJSON(courseJSON: any, subject: Subject, shouldFetchSectionData: boolean): Course
-  {
-    var newCourseObject = Course.fromJSON(courseJSON, subject)
+  static handleCourseJSON(
+    courseJSON: any,
+    subject: Subject,
+    shouldFetchSectionData: boolean
+  ): Course {
+    const newCourseObject = Course.fromJSON(courseJSON, subject);
 
-    if (shouldFetchSectionData)
-    {
-      var courseSections: Section[] = APIManager.handleCourseSectionsJSON(courseJSON.classSections)
-      newCourseObject.createLectures(courseSections)
+    if (shouldFetchSectionData) {
+      const courseSections: Section[] = APIManager.handleCourseSectionsJSON(
+        courseJSON.classSections
+      );
+      newCourseObject.createLectures(courseSections);
     }
 
-    return newCourseObject
+    return newCourseObject;
   }
 
-  static handleCourseSectionsJSON(courseSectionsJSON: any): Section[]
-  {
+  static handleCourseSectionsJSON(courseSectionsJSON: any): Section[] {
     return (courseSectionsJSON || []).map((courseSectionJSON: any) => {
-      return Section.fromJSON(courseSectionJSON)
-    })
+      return Section.fromJSON(courseSectionJSON);
+    });
   }
 }
--- api/api-tests.ts
+++ api/api-tests.ts
@@ -1,20 +1,30 @@
-import { APIManager } from "./api-manager"
-import { YearQuarter, Quarter, CourseID } from "../shared/model/model"
+import { APIManager } from "./api-manager";
+import { CourseID, Quarter, YearQuarter } from "../shared/model/model";
 
 export var apiTests = async () => {
   // Block for API testing
-  const subjectCodeToFetch = "CMPSC"
-  const quarterToFetch = new YearQuarter(2022, Quarter.Winter)
-  const courseIDToFetch = new CourseID(subjectCodeToFetch, "", "154", "")
+  const subjectCodeToFetch = "CMPSC";
+  const quarterToFetch = new YearQuarter(2022, Quarter.Winter);
+  const courseIDToFetch = new CourseID(subjectCodeToFetch, "", "154", "");
 
-  var subjectArray = await APIManager.fetchSubjects()
-  console.log(subjectArray)
-  var testSubject = subjectArray.find(subject => subject.code == subjectCodeToFetch)!
-  console.log(testSubject)
-  var courseArray = await APIManager.fetchCourses(quarterToFetch, testSubject, false)
-  console.log(courseArray)
-  var course = await APIManager.fetchCourseFromObject(courseArray[0])
-  console.log(course)
-  var course2 = await APIManager.fetchCourse(quarterToFetch, courseIDToFetch, testSubject)
-  console.log(course2)
-}
+  const subjectArray = await APIManager.fetchSubjects();
+  console.log(subjectArray);
+  const testSubject = subjectArray.find(
+    (subject) => subject.code === subjectCodeToFetch
+  )!;
+  console.log(testSubject);
+  const courseArray = await APIManager.fetchCourses(
+    quarterToFetch,
+    testSubject,
+    false
+  );
+  console.log(courseArray);
+  const course = await APIManager.fetchCourseFromObject(courseArray[0]);
+  console.log(course);
+  const course2 = await APIManager.fetchCourse(
+    quarterToFetch,
+    courseIDToFetch,
+    testSubject
+  );
+  console.log(course2);
+};
