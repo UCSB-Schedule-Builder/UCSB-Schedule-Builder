6c47c43aa0a5bd611c9fd93276c32917617c798f
LMAO-ZEDONG <djerez@ucsb.edu>
2021-11-18 03:06:03 -0800

    Change hover gradient, Replace "is-selected" with "units"

--- components/SearchResultCell.tsx
+++ components/SearchResultCell.tsx
@@ -1,12 +1,12 @@
 function SearchResultCell({id, title}: {id:string, title:string}) {
+  //main gradient: linear-gradient(135deg, #2e22ac 0%, #ce448d 100%)
   return(
     <div className="search-result-cell">
       <div className="course-ID">{id}</div>
       <div className="course-title">{title}</div>
-      <div className="is-selected">placeholder for selected icon</div>
+      {<div className="units">Units</div>}
 
       <style jsx>{`
-
         .search-result-cell {
           display: flex;
           justify-content: space-between;
@@ -15,10 +15,12 @@ function SearchResultCell({id, title}: {id:string, title:string}) {
           border-radius: 5px;
           padding: 20px;
           margin: 3px;
+          cursor: pointer;
         }
 
         .search-result-cell:hover {
-          background: linear-gradient(135deg, #2e22ac 0%, #ce448d 100%)
+          background: linear-gradient(135deg, #6e62ec 0%, #ee84cd 100%)
+
             no-repeat fixed;
           color: #fefefe;
 
