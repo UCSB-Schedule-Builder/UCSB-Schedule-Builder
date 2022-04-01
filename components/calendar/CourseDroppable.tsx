40ad42985a619dadb88d45489f06b55714718765
jacksonjude <jacksonjude@mac.com>
2022-04-01 01:56:07 -0700

    Adjusted CourseDroppable positioning for reworked grid frame

--- components/calendar/CourseDroppable.tsx
+++ components/calendar/CourseDroppable.tsx
@@ -18,18 +18,21 @@ export function CourseDroppable({timeslot, courseID} : CourseDroppableProps)
 
   return (
     <>
-      <div style={{}}>
-        <Droppable droppableId={timeslot.enrollCode} type={courseID.toString()} children={() => (
-          <div style={{width: width/columnCount, height: getSlotHeight(mainStartTime, mainEndTime), top: timeToYPos(mainStartTime)+weekdayLabelMargin+5, left: dayLetterToXPos(mainDayOfWeek.letter)+timeLabelMargin+4, position: "absolute", backgroundColor: "blue"}}></div>
-        )} />
-      </div>
       <div>
         {timeslot.times.flatMap(courseTime => {
           if (!courseTime.startTime || !courseTime.endTime) { return }
           return courseTime.days.flatMap((dayOfWeek, index) => {
-            if (index == 0) { return [] }
             return (
-              <div style={{width: width/columnCount, height: getSlotHeight(courseTime.startTime!, courseTime.endTime!), top: timeToYPos(courseTime.startTime!)+weekdayLabelMargin+5, left: dayLetterToXPos(dayOfWeek.letter)+timeLabelMargin+4, position: "absolute", backgroundColor: "blue"}}></div>
+              <Droppable droppableId={timeslot.enrollCode} type={courseID.toString()} children={() => (
+                <div style={{
+                  width: width/columnCount-4,
+                  height: getSlotHeight(courseTime.startTime!, courseTime.endTime!),
+                  top: timeToYPos(courseTime.startTime!)+weekdayLabelMargin+5,
+                  left: dayLetterToXPos(dayOfWeek.letter)+timeLabelMargin+4,
+                  position: "absolute",
+                  backgroundColor: "blue"
+                }}></div>
+              )} />
             )
           })
         })}
